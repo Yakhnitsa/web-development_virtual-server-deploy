7777b64911752bea177d5fd80007c555f0f459a1
Yakhnitsa <transpmailbox@gmail.com>
2020-05-23 11:46:42 +0300

    Initial commit

---
+++ src/main/java/com/example/webdevelopment_virtualserverdeploy/WebDevelopmentVirtualServerDeployApplication.java
@@ -0,0 +1,13 @@
+package com.example.webdevelopment_virtualserverdeploy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebDevelopmentVirtualServerDeployApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WebDevelopmentVirtualServerDeployApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/webdevelopment_virtualserverdeploy/WebDevelopmentVirtualServerDeployApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.webdevelopment_virtualserverdeploy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebDevelopmentVirtualServerDeployApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
